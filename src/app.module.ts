ac9d888e9ab2779b432af0980c557eb9a453b503
Johan <57840332+johannasendi@users.noreply.github.com>
2022-03-01 21:44:17 +0900

    connection to database
    Mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import 'dotenv/config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PW,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: true,
+    }),
+  
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
